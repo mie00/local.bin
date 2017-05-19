561b1bbf25f3984208d56e1d498b04054826b455
Mohamed Elawadi <mohamed@elawadi.net>
2017-05-20 00:13:22 +0200

    vlcwrapper: show thumbnail

--- vlcwrapper.py
+++ vlcwrapper.py
@@ -5,9 +5,35 @@ import subprocess
 from time import sleep
 import threading
 import time
+import curses
+import urllib.parse
+from cursesmenu import CursesMenu, SelectionMenu
+
+class MySelectionMenu(SelectionMenu):
+    def __init__(self, *args, cb=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cb = cb
+
+    def _main_loop(self, scr):
+        if scr is not None:
+            CursesMenu.stdscr = scr
+        self.screen = curses.newpad(len(self.items) + 6, CursesMenu.stdscr.getmaxyx()[1])
+        self._set_up_colors()
+        curses.curs_set(0)
+        CursesMenu.stdscr.refresh()
+        self.draw()
+        CursesMenu.currently_active_menu = self
+        self._running.set()
+        while self._running.wait() is not False and not self.should_exit:
+            ui = self.process_user_input()
+            if self.cb:
+                self.cb(self, ui)
+
+
 
 HOME = os.environ['HOME']
 CACHE = '{}/.cache/vlcwrapper'.format(HOME)
+THUMBS = '{}/thumbs'.format(CACHE)
 
 class Played(object):
     def __init__(self, fpath):
@@ -31,7 +57,7 @@ class Played(object):
 
     def get_metadata(self):
         try:
-            res = [subprocess.check_output(['playerctl', 'metadata', 'xesam:title']).decode().strip(), subprocess.check_output(['playerctl', 'position']).decode().strip()]
+            res = [subprocess.check_output(['playerctl', 'metadata', 'xesam:title']).decode().strip(), subprocess.check_output(['playerctl', 'position']).decode().strip(), subprocess.check_output(['playerctl', 'metadata', 'xesam:url']).decode()]
             return res
         except:
             running = False
@@ -86,32 +112,71 @@ def play(path):
     played.start()
 
 def choise():
-    from cursesmenu import SelectionMenu
     elems = []
+    thumbs = os.listdir(THUMBS)
     for name in os.listdir(CACHE):
+        if not name.startswith('!'):
+            continue
         path = name.replace('!', '/')
         f = os.path.join(CACHE, name)
         mtime = os.stat(f).st_mtime
         with open(f) as ff:
             curr = ff.readline().strip('\n')
             pos = ff.readline().strip('\n')
+            t = '{}:{:02d}'.format(int(float(pos)//60), int(float(pos)%60))
+            fname = ff.readline().strip('\n')
         if curr:
-            elems.append((mtime, path, curr, float(pos)))
+            thumbpre = name + '!'
+            thumbname = '{}!{}{}.png'.format(thumbpre, curr, pos)
+            found = False
+            for thumb in thumbs[:]:
+                if thumb.startswith(thumbpre):
+                    if thumb == thumbname:
+                        found = True
+                    else:
+                        os.remove(os.path.join(THUMBS, thumb))
+                    break
+            if found == False:
+                fname = fname[len('file://'):] if fname.startswith('file://') else fname
+                fname = urllib.parse.unquote(fname)
+                c = subprocess.call(['ffmpeg', '-ss' , t, '-i', fname , '-vf', 'scale=320:-1', '-q:v', '4', '-vframes', '1', os.path.join(THUMBS, thumbname)])
+                if c == 0:
+                    found = True
 
-    files = ('{} {}:{:02d} |{}'.format(k, int(l//60), int(l%60), j)  for _, j, k, l in reversed(sorted(elems)))
-    menu = SelectionMenu(files, title='Recently Played')
+            thumb = os.path.join(THUMBS, thumbname) if found else ''
+            elems.append((mtime, path, curr, t, thumb))
+
+    files = []
+    sitems = list(reversed(sorted(elems)))
+    for _, j, k, l, _ in sitems:
+        files.append('{} {} |{}'.format(k, l, j))
+    def image(menu, x):
+        if x == ord('s') and menu.current_option < len(sitems):
+            t = sitems[menu.current_option][4]
+            if t:
+                subprocess.call(['pxl', t])
+                menu.clear_screen()
+                os.system('reset')
+                curses.noecho()
+                curses.cbreak()
+                menu.stdscr.keypad(True)
+                menu.draw()
+        if x == ord('q'):
+            menu.go_to(len(sitems))
+            menu.select()
+    menu = MySelectionMenu(files, title='Recently Played', cb=image)
     menu.show()
-    s = menu.selected_item.text
-    if s == 'Exit':
-        return s
+    s = menu.selected_option
+    if s >= len(sitems):
+        return None
     else:
-        return s.split('|')[1]
+        return sitems[s][1]
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         play(sys.argv[1])
     else:
         c = choise()
-        if c != 'Exit':
+        if c is not None:
             play(c)
 
